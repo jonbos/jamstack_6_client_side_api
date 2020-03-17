347dbab1fa73fabd6deb19f15d3e91ce7f764b1b
Jon Bos <bos.jon@gmail.com>
2020-03-17 16:21:23 -0500

    adds https config to eleventy

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,16 @@
 module.exports = function(config) {
+  config.setBrowserSyncConfig({
+    https: {
+      key: "/etc/localhost.key",
+      cert: "/etc/localhost.crt"
+    }
+  });
   config.addPassthroughCopy("src/js");
   return {
     dir: {
       input: "src",
       output: "dist",
-      data : "_data"
+      data: "_data"
     }
   };
 };
