138cc70a0a53a05a0b68a4471849dbda85305ec5
Jon Bos <bos.jon@gmail.com>
2020-03-19 14:11:39 -0500

    Moves weather to serverless function

--- src/js/weather.js
+++ src/js/weather.js
@@ -1,6 +1,5 @@
 function success(pos) {
-  const WEATHER_API_KEY = "b31c0c067c03dc02f682084b6a184f53";
-  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${pos.coords.latitude}&lon=${pos.coords.longitude}&appid=${WEATHER_API_KEY}&units=imperial`;
+  const url = `/.netlify/functions/weatherapi?lat=${pos.coords.latitude}&lon=${pos.coords.longitude}&units=imperial`;
   fetch(url)
     .then(response => {
       return response.json();
@@ -10,7 +9,6 @@ function success(pos) {
       document.querySelector("#temp").textContent = data.main.temp + "°F";
       document.querySelector("#main").textContent = data.weather[0].main;
       document.querySelector("#desc").textContent = data.weather[0].description;
-      
       // progressive enhancement - only display div if successful request is made
       document.querySelector("#weather").classList.remove("hidden");
     });
