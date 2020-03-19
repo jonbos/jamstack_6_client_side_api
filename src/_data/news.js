fe05420d4a1b679db222b59ffa0303d53b995e59
Jon Bos <bos.jon@gmail.com>
2020-03-19 14:27:09 -0500

    all countries, rm logging

--- src/_data/news.js
+++ src/_data/news.js
@@ -18,7 +18,6 @@ async function getNews(country) {
 module.exports = async function() {
   var newsPromises = countries.map(getNews);
   return Promise.all(newsPromises).then(newsObjects => {
-    console.log('newsObjects', newsObjects);
     return [].concat.apply([], newsObjects);
   });
 };
